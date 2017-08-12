0b8a767c3c2bb6b8c22260c6fb5bd5545ca0d666
marius teuguia <marius.dombouteuguia@condorcet.be>
2017-08-12 19:19:55 +0200

    mise  en place des beans, dao, et du premier servlet login

---
+++ WebContent/vues/login.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="login.php" method="post">
+		<table>
+			<tr>
+				<td><label>login</label></td>
+				<td><input type="text" name="txtlogin"></td>
+			</tr>
+			<tr>
+				<td><label>password</label></td>
+				<td><input type="text" name="txtpassword"></td>
+			</tr>
+			<tr>
+				<td> moi</td>
+				<td><input type="submit" value="login"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ src/servlets/login.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/vues/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String login =(String)request.getParameter("txtlogin");
+		request.setAttribute("txtlogin", login);
+		
+		this.getServletContext().getRequestDispatcher("/vues/dashboardClient.jsp").forward(request, response);
+	}
+
+}
